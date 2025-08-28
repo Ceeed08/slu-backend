4dfaf6e4518517aa6ff7ac74caa72e272d4972d5
Ceeed <cedrickagapito@gmail.com>
2025-08-28 17:12:24 +0800

    modified item.js and added a way to fetch data

--- api/models/Item.js
+++ api/models/Item.js
@@ -1,13 +1,27 @@
+// import mongoose from 'mongoose';
+
+// const ItemSchema = new mongoose.Schema({
+//   name: { type: String, required: true },
+//   category: {type: String},
+//   count: {type: Number},
+//   image: { type: String, trim: true },
+//   thumbnails: [{ type: String, trim: true }],
+//   shortDescription: { type: String },
+//   instructions: { type: String },
+// }, { timestamps: true });
+
+// export default mongoose.models.Item || mongoose.model('Item', ItemSchema);
 import mongoose from 'mongoose';
 
 const ItemSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  category: {type: String},
-  count: {type: Number},
+  category: { type: String },
+  count: { type: Number },
   image: { type: String, trim: true },
   thumbnails: [{ type: String, trim: true }],
   shortDescription: { type: String },
   instructions: { type: String },
+  isActive: { type: Boolean, default: true }, // Add this new field
 }, { timestamps: true });
 
 export default mongoose.models.Item || mongoose.model('Item', ItemSchema);
